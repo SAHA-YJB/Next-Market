f1c433fbc8e1283dcc7a3abb64e716251336f8d9
SAHA-YJB <pkomom123@naver.com>
2023-11-26 12:13:54 +0900

    features: nextAuth setting

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavItem = ({ mobile }: { mobile?: boolean }) => {
+  const { data: session, status } = useSession();
+  console.log({ session }, status);
   return (
     <ul
       className={`text-md flex justify-center gap-4 w-full items-center 
@@ -13,12 +17,15 @@ const NavItem = ({ mobile }: { mobile?: boolean }) => {
       <li className="py-2 text-center border-b-4 cursor-pointer">
         <Link href="/user">User</Link>
       </li>
-      <li className="py-2 text-center border-b-4 cursor-pointer">
-        <button>Logout</button>
-      </li>
-      <li className="py-2 text-center border-b-4 cursor-pointer">
-        <button>Login</button>
-      </li>
+      {session?.user ? (
+        <li className="py-2 text-center border-b-4 cursor-pointer">
+          <button onClick={() => signOut()}>Logout</button>
+        </li>
+      ) : (
+        <li className="py-2 text-center border-b-4 cursor-pointer">
+          <button onClick={() => signIn()}>Login</button>
+        </li>
+      )}
     </ul>
   );
 };
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,53 @@
+//넥스트 어스는 아직은 app안에 api폴더에서 사용 불가 일단은 pages폴더에 api폴더를 만들어서 사용
+import NextAuth, {NextAuthOptions} from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import CredentialsProvider from "next-auth/providers/credentials"
+import KakaoProvider from "next-auth/providers/kakao";
+
+const prisma = new PrismaClient()
+
+export const authOptions: NextAuthOptions = ({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+  
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+  
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    })
+  ],
+  session: {
+    strategy: "jwt",
+  }
+})
+export default NextAuth(authOptions)
