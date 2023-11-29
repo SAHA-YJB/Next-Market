0e7c080ef1f7321dbb13db04bb4298bf863205f4
SAHA-YJB <pkomom123@naver.com>
2023-11-30 08:59:45 +0900

    features: imageUpload ui and basic logic create

--- src/app/products/upload/page.tsx
+++ src/app/products/upload/page.tsx
@@ -2,6 +2,7 @@
 import Button from '@/components/Button';
 import Container from '@/components/Container';
 import Heading from '@/components/Heading';
+import ImageUpload from '@/components/ImageUpload';
 import Input from '@/components/Input';
 import React, { useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
@@ -30,6 +31,13 @@ const ProductUploadPage = () => {
     },
   });
 
+  // watch 함수는 특정 입력 필드의 상태를 실시간으로 관찰하고 그 값을 반환하는 역할
+  const imgSrc = watch('imageSrc');
+  //밸류는 여러가지가 될 수 있다
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value);
+  };
+
   //각 인풋에 대한 입력값을 data매개변수가 받음
   const onSubmit: SubmitHandler<FieldValues> = (data) => {};
 
@@ -38,6 +46,10 @@ const ProductUploadPage = () => {
       <div className='max-w-screen-lg mx-auto'>
         <form className='flex flex-col gap-8' onSubmit={handleSubmit(onSubmit)}>
           <Heading title='Product Upload' subtitle='Upload Your Product' />
+          <ImageUpload
+            onChange={(value) => setCustomValue('imageSrc', value)}
+            value={imgSrc}
+          />
           <Input
             id='title'
             label='Title'
