74cb6ff8f05851730f31eac9f57f018c5815969b
SAHA-YJB <pkomom123@naver.com>
2023-12-01 11:38:53 +0900

    features: like DELETE api create

--- src/app/api/favorites/[productId]/route.ts
+++ src/app/api/favorites/[productId]/route.ts
@@ -27,6 +27,25 @@ export async function POST(request: Request, { params }: { params: params }) {
 
   return NextResponse.json(user);
 }
+// // // // // // //// // // // // // //// // // // // // //// // // // // // //
+export async function DELETE(request: Request, { params }: { params: params }) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return Response.redirect('/auth/login');
+  }
 
-// //
-// export async function DELETE() {}
+  const { productId } = params;
+  if (!productId || typeof productId !== 'string') {
+    return new Error('유효하지 않은 아이디입니다.');
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+  favoriteIds = favoriteIds.filter((id) => id !== productId);
+
+  const user = await prisma?.user.update({
+    where: { id: currentUser.id },
+    data: { favoriteIds },
+  });
+
+  return NextResponse.json(user);
+}
