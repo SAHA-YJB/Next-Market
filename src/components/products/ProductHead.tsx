09f083c7f255a015a3ed25445fde46704ba86057
SAHA-YJB <pkomom123@naver.com>
2023-12-03 19:43:50 +0900

    features: productHeader component create

--- src/components/products/ProductHead.tsx
+++ src/components/products/ProductHead.tsx
@@ -1,7 +1,38 @@
+import { User } from '@prisma/client';
 import React from 'react';
+import Heading from '../Heading';
+import Image from 'next/image';
+import HeartButton from '../HeartButton';
 
-const ProductHead = () => {
-  return <div>ProductHead</div>;
+interface ProductHeadProps {
+  title: string;
+  imageSrc: string;
+  id: string;
+  currentUser?: User | null;
+}
+
+const ProductHead = ({
+  title,
+  imageSrc,
+  id,
+  currentUser,
+}: ProductHeadProps) => {
+  return (
+    <>
+      <Heading title={title} />
+      <div className='w-full h-[60vh] overflow-hidden rounded-xl relative'>
+        <Image
+          fill
+          src={imageSrc}
+          alt='product'
+          className='object-cover w-full'
+        />
+        <div className='absolute top-5 right-5'>
+          <HeartButton productId={id} currentUser={currentUser} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProductHead;
