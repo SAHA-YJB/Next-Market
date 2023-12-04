a48b2fce37aca3f2e4cd8ed44f482ba3318a4c01
SAHA-YJB <pkomom123@naver.com>
2023-12-04 10:39:13 +0900

    features: chat data create getReq

--- src/app/chat/ChatClient.tsx
+++ src/app/chat/ChatClient.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { User } from '@prisma/client';
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 interface ChatClientProps {
   currentUser?: User | null;
@@ -13,6 +14,10 @@ const ChatClient = ({ currentUser }: ChatClientProps) => {
     receiverImage: '',
   });
   const [layout, setLayout] = useState(false);
+
+  useEffect(() => {
+    axios.get('/api/chat').then((res) => console.log(res));
+  }, []);
 
   return (
     <main>
