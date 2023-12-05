58a9a152fb27d20239de314e8919c1d76b916142
SAHA-YJB <pkomom123@naver.com>
2023-12-05 09:17:17 +0900

    features: user components create

--- src/components/chat/User.tsx
+++ src/components/chat/User.tsx
@@ -1,7 +1,47 @@
+import { TConversation, TUserWithChat } from '@/types';
 import React from 'react';
+import Avatar from '../Avatar';
+import { fromNow } from '@/helpers/dayjs';
 
-const User = ({ user, currentUserId }) => {
-  return <div>User</div>;
+interface UserProps {
+  user: TUserWithChat;
+  currentUserId: string;
+}
+
+const User = ({ user, currentUserId }: UserProps) => {
+  const messagesWithCurrentUser = user.conversations.find(
+    (conversation: TConversation) => {
+      conversation.users.find((user) => user.id === currentUserId);
+    }
+  );
+
+  const latestMessage = messagesWithCurrentUser?.messages.slice(-1)[0];
+
+  return (
+    <div className='grid grid-cols-[40px_1fr_5px] grid-rows-[40px] gap-3 py-3 px-4 border-b-[1px] hover:cursor-pointer hover:bg-green-300'>
+      <div>
+        <Avatar src={user.image} />
+      </div>
+      <div>
+        <h3>{user.name}</h3>
+        {latestMessage && (
+          <p className='overflow-hidden text-xs font-medium text-gray-600 break-words whitespace-pre-wrap'>
+            {latestMessage.text}
+          </p>
+        )}
+        {latestMessage && latestMessage?.image && (
+          <p className='text-xs font-medium text-gray-600'>사진</p>
+        )}
+      </div>
+      <div>
+        {latestMessage && (
+          <p className='text-xs font-medium text-gray-600'>
+            {fromNow(latestMessage.createdAt)}
+          </p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default User;
